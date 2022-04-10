f1e91d7d8a9895025f1a9d5c184f5b466fe74496
Paradigm0001 <46477191+Paradigm0001@users.noreply.github.com>
2022-04-10 04:27:07 +0000

    Update installSpotify.sh

--- installSpotify.sh
+++ installSpotify.sh
@@ -3,6 +3,6 @@ sudo xbps-install -Syu
 sudo xbps-install -Syu
 sudo xbps-install git xtools
 cd void-packages
-sudo -u nobody ./xbps-src binary-bootstrap
-sudo -u nobody ./xbps-src pkg spotify
+sudo -u '#1000' ./xbps-src binary-bootstrap
+sudo -u '#1000' ./xbps-src pkg spotify
 sudo xbps-install --repository=hostdir/binpkgs/nonfree spotify

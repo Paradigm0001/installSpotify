1ebed6251997953c1723bdfd8959e11225fb3457
Unixparadigm <unixparadigm@protonmail.com>
2022-04-09 17:14:43 -0700

    void-packages

--- installSpotify.sh
+++ installSpotify.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-cd /tmp
 sudo xbps-install -Syu
 sudo xbps-install -Syu
 sudo xbps-install git xtools
-sudo rm -rf void-packages || true
-sudo -u nobody git clone https://github.com/void-linux/void-packages.git
 cd void-packages
 sudo -u nobody ./xbps-src binary-bootstrap
-echo XBPS_ALLOW_RESTRICTED=yes | sudo tee etc/conf
 sudo -u nobody ./xbps-src pkg spotify
 sudo xbps-install --repository=hostdir/binpkgs/nonfree spotify
